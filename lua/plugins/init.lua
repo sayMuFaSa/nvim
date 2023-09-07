5ecbdcb8783878a0f86b07056e364af30447be76
farid <faridfaracli9@gmail.com>
2023-09-07 14:04:14 +0400

    Some changes in colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,50 +1,9 @@
-local fn = vim.fn
+vim.cmd("packadd packer.nvim")
 
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
-end
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use {'wbthomason/packer.nvim'}
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
-
--- Install your plugins here
-return packer.startup(function(use)
-	use ("wbthomason/packer.nvim") -- Have packer manage itself	
-
-	require('packer').startup(function(use)
-		use({ 'rose-pine/neovim', as = 'rose-pine' })
-	end)
 
 	
 	use { "catppuccin/nvim", as = "catppuccin" }
@@ -83,16 +42,13 @@ return packer.startup(function(use)
 	
 
 
-}
+	}
 
 
 
 
 
 
-if PACKER_BOOTSTRAP then
-	require("packer").sync()
-end
 end)
 
 
